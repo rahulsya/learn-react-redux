d9a0b11e9edba56b91c83999239ec70f5c2e1a5e
rahulsya <rahulsyaban@gmail.com>
2020-08-12 14:56:47 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const initialState = {
+  cart: cartItems,
+  tota: 0,
+  amount: 0,
+};
+
+const store = createStore(reducer, initialState);
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
